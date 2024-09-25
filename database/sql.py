ad03780e87b35248a69bd9422255e85ebb4fea38
Sofiana hanifah Aprilia <96537396+Xyren-64bit@users.noreply.github.com>
2024-09-26 00:17:31 +0800

    Update sql.py

--- database/sql.py
+++ database/sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from sqlalchemy import TEXT, Column, Numeric, create_engine
+from sqlalchemy import TEXT, Column, Numeric, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
