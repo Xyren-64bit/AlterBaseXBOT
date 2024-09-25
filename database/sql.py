5c96f95e8906230160dacbc93f1ed26736969346
Sofiana hanifah Aprilia <96537396+Xyren-64bit@users.noreply.github.com>
2024-09-26 00:26:31 +0800

    Update sql.py

--- database/sql.py
+++ database/sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from sqlalchemy import TEXT, Column, Numeric, Integer, DateTime, create_engine
+from sqlalchemy import TEXT, Column, Numeric, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -60,36 +60,3 @@ async def query_msg():
         return SESSION.query(Broadcast.id).order_by(Broadcast.id)
     finally:
         SESSION.close()
-
-class ScheduledPost(BASE):
-    __tablename__ = "scheduled_post"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    message_id = Column(Numeric)
-    scheduled_time = Column(DateTime)
-
-    def __init__(self, message_id, scheduled_time):
-        self.message_id = message_id
-        self.scheduled_time = scheduled_time
-
-
-ScheduledPost.__table__.create(checkfirst=True)
-
-
-async def add_scheduled_post(message_id, scheduled_time):
-    with INSERTION_LOCK:
-        post = ScheduledPost(message_id, scheduled_time)
-        SESSION.add(post)
-        SESSION.commit()
-
-
-async def get_scheduled_posts():
-    try:
-        return SESSION.query(ScheduledPost).all()
-    finally:
-        SESSION.close()
-
-
-async def delete_scheduled_post(message_id):
-    with INSERTION_LOCK:
-        SESSION.query(ScheduledPost).filter(ScheduledPost.message_id == message_id).delete()
-        SESSION.commit()
